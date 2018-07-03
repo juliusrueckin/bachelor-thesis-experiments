ea20d8a05981042c946055752d688e07912e4451
Julius Rückin <julius.rueckin@student.hpi.uni-potsdam.de>
2018-07-03 19:12:42 +0200

    add multi-label classification experiment class

--- benchmarks.py
+++ benchmarks.py
@@ -5,9 +5,12 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics.cluster import normalized_mutual_info_score
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import MultiLabelBinarizer
 
 
+# multi-class classification through logistic regression
+# wrapper for customizable initialization, training, prediction and evaluation
 class MultiClassClassification:
 
 	start_time = None
@@ -73,6 +76,7 @@ class MultiClassClassification:
 
 		return self.logistic_regression_model
 
+	# predict class of each sample, based on pre-trained model
 	def predict(self):
 		print('Predict multi-class classification experiment with {} on {} evaluated through {} on {}% train data!'
 			.format(self.method_name, self.dataset_name, self.performance_function, self.train_size * 100.00))
@@ -88,7 +92,7 @@ class MultiClassClassification:
 
 		return self.node_label_predictions
 
-	# evaluate prediction results through already pre-defined performance function(s)return results as a dict
+	# evaluate prediction results through already pre-defined performance function(s), return results as a dict
 	def evaluate(self):
 		print('Evaluate multi-class classification experiment with {} on {} evaluated through {} on {}% train data!'
 			.format(self.method_name, self.dataset_name, self.performance_function, self.train_size * 100.00))
@@ -104,6 +108,109 @@ class MultiClassClassification:
 			results['micro'] = f1_score(self.node_labels_test, self.node_label_predictions, average='micro')
 
 		return results
+
+
+# multi-label classification through k-nearest neighbor classifier
+# wrapper for customizable initialization, training, prediction and evaluation
+class MultiLabelClassification:
+	start_time = None
+	end_time = None
+
+	# algorithm configurations
+	method_name = 'Verse-PPR'
+	dataset_name = 'Test-Data'
+	performance_function = 'Macro-F1'
+	train_size = 0.3
+	n_neighbors = 5
+
+	# performance evaluation methods
+	MACRO_F1 = 'macro_f1'
+	MICRO_F1 = 'micro_f1'
+	BOTH = 'both'
+
+	# feature and label space, where each sample has a set of classes
+	embeddings = []
+	node_labels = []
+
+	# train-test split of feature and label space
+	embeddings_train = []
+	node_labels_train = []
+	embeddings_test = []
+	node_labels_test = []
+
+	# model and its prediction
+	nearest_neighbors = None
+	node_label_predictions = []
+
+	# initialize classification algorithm with customized configuration parameters
+	# produce random train-test split
+	def __init__(self, method_name='Verse-PPR', dataset_name='Test-Data', performance_function='both',
+				 train_size=0.3, embeddings=None, node_labels=None, n_neighbors=5):
+		print('Initialize multi-label classification experiment with {} on {} evaluated through {} on {}% train data!'
+			  .format(method_name, dataset_name, performance_function, train_size * 100.00))
+
+		self.method_name = method_name
+		self.dataset_name = dataset_name
+		self.performance_function = performance_function
+		self.train_size = train_size
+		self.embeddings = embeddings
+		self.node_labels = MultiLabelBinarizer().fit_transform(node_labels)
+		self.n_neighbors = n_neighbors
+
+		self.embeddings_train, self.embeddings_test, self.node_labels_train, self.node_labels_test = \
+			train_test_split(self.embeddings, self.node_labels, train_size=train_size, test_size=1 - train_size)
+
+	# train through k-nearest neighbor classifier
+	def train(self):
+		print('Train multi-label classification experiment with {} on {} evaluated through {} on {}% train data!'
+			  .format(self.method_name, self.dataset_name, self.performance_function, self.train_size * 100.00))
+
+		self.start_time = time.time()
+
+		self.nearest_neighbors = KNeighborsClassifier(n_neighbors=self.n_neighbors, weights='distance', algorithm='auto',
+													  metric='cosine', n_jobs=-1)
+		self.nearest_neighbors.fit(self.embeddings_train, self.node_labels_train)
+
+		self.end_time = time.time()
+
+		total_train_time = round(self.end_time - self.start_time, 2)
+		print('Trained multi-label classification experiment in {} sec.!'.format(total_train_time))
+
+		return self.nearest_neighbors
+
+	# predict class of each sample, based on pre-trained model
+	def predict(self):
+		print('Predict multi-label classification experiment with {} on {} evaluated through {} on {}% train data!'
+			  .format(self.method_name, self.dataset_name, self.performance_function, self.train_size * 100.00))
+
+		self.start_time = time.time()
+
+		self.node_label_predictions = self.nearest_neighbors.predict(self.embeddings_test)
+
+		self.end_time = time.time()
+
+		total_prediction_time = round(self.end_time - self.start_time, 2)
+		print('Predicted multi-label classification experiment in {} sec.!'.format(total_prediction_time))
+
+		return self.node_label_predictions
+
+	# evaluate prediction results through already pre-defined performance function(s), return results as a dict
+	def evaluate(self):
+		print('Evaluate multi-label classification experiment with {} on {} evaluated through {} on {}% train data!'
+			  .format(self.method_name, self.dataset_name, self.performance_function, self.train_size * 100.00))
+
+		results = {}
+
+		if self.performance_function == self.BOTH:
+			results['macro'] = f1_score(self.node_labels_test, self.node_label_predictions, average='macro')
+			results['micro'] = f1_score(self.node_labels_test, self.node_label_predictions, average='micro')
+		elif self.performance_function == self.MACRO_F1:
+			results['macro'] = f1_score(self.node_labels_test, self.node_label_predictions, average='macro')
+		elif self.performance_function == self.MICRO_F1:
+			results['micro'] = f1_score(self.node_labels_test, self.node_label_predictions, average='micro')
+
+		return results
+
 
 
 class Clustering:
@@ -145,6 +252,7 @@ class Clustering:
 		self.node_labels = node_labels
 		self.n_clusters = n_clusters
 
+	# train clustering through k-means approach
 	def train(self):
 		print('Train clustering experiment with {} on {} evaluated through {}!'
 			  .format(self.method_name, self.dataset_name, self.performance_function))
@@ -161,6 +269,7 @@ class Clustering:
 
 		return self.k_means
 
+	# predict clustering of each sample, based on nearest centroid
 	def predict(self):
 		print('Predict clustering experiment with {} on {} evaluated through {}!'
 			  .format(self.method_name, self.dataset_name, self.performance_function))
@@ -176,6 +285,7 @@ class Clustering:
 
 		return self.node_label_predictions
 
+	# evaluate clustering quality through already pre-defined performance function(s), return results as a dict
 	def evaluate(self):
 		print('Evaluate clustering experiment with {} on {} evaluated through {}!'
 				.format(self.method_name, self.dataset_name, self.performance_function))
