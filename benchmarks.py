935c6fe4e7db3d8d159a5c7405bb087720cdd6bc
Julius Rückin <julius.rueckin@student.hpi.uni-potsdam.de>
2018-07-03 21:13:28 +0200

    add link prediction experiment class. TODO: implement train-test split and implement logistic regression with negative sampling

--- benchmarks.py
+++ benchmarks.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -8,6 +9,155 @@ from sklearn.metrics import silhouette_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.multiclass import OneVsRestClassifier
+
+
+# link prediction through logistic regression on edgewise features
+# wrapper for customizable initialization, training, prediction and evaluation
+class LinkPrediction:
+
+	start_time = None
+	end_time = None
+
+	# algorithm configurations
+	method_name = 'Verse-PPR'
+	dataset_name = 'Test-Data'
+	performance_function = 'Macro-F1'
+	train_size = 0.5
+	vector_operator = 'hadamard'
+
+	# performance evaluation methods
+	MACRO_F1 = 'macro_f1'
+	MICRO_F1 = 'micro_f1'
+	BOTH = 'both'
+
+	# vector operators
+	AVERAGE = 'avg'
+	CONCAT = 'concat'
+	HADAMARD = 'hadamard'
+	WEIGHTED_L1 = 'weighted_l1'
+	WEIGHTED_L2 = 'weighted_l2'
+
+	# node and edge embedding feature space, and new edges list
+	node_embeddings = []
+	edge_embeddings = []
+	new_edges = []
+
+	# train-test split of new edges feature space
+	edge_embeddings_train = []
+	edge_embeddings_test = []
+
+	# initialize classification algorithm with customized configuration parameters
+	# compute new edge feature space
+	# randomly split new edges feature space in one half train and test set
+	def __init__(self, method_name='Verse-PPR', dataset_name='Test-Data', performance_function='both',
+				 node_embeddings=None, new_edges=None, vector_operator='hadamard'):
+		print(
+			'Initialize link prediction experiment with {} on {} evaluated through {} on {}% train data!'
+			.format(method_name, dataset_name, performance_function, self.train_size * 100.00))
+
+		self.method_name = method_name
+		self.dataset_name = dataset_name
+		self.performance_function = performance_function
+		self.node_embeddings = node_embeddings
+		self.new_edges = new_edges
+		self.vector_operator = vector_operator
+
+		self.compute_new_edges_feature_space()
+		self.new_edges_train_test_split()
+
+	def new_edges_train_test_split(self):
+		self.edge_embeddings_train = self.edge_embeddings[]
+		self.edge_embeddings_test = []
+
+	# compute new edge feature space based on configured vector operator
+	def compute_new_edges_feature_space(self):
+		for edge in self.new_edges:
+			n1 = np.array(self.node_embeddings[edge[0]])
+			n2 = np.array(self.node_embeddings[edge[1]])
+
+			if self.vector_operator == self.AVERAGE:
+				self.edge_embeddings.append(self.average_op(n1, n2))
+			elif self.vector_operator == self.CONCAT:
+				self.edge_embeddings.append(self.concat_op(n1, n2))
+			elif self.vector_operator == self.HADAMARD:
+				self.edge_embeddings.append(self.hadamard_op(n1, n2))
+			elif self.vector_operator == self.WEIGHTED_L1:
+				self.edge_embeddings.append(self.weighted_l1_op(n1, n2))
+			elif self.vector_operator == self.WEIGHTED_L2:
+				self.edge_embeddings.append(self.weighted_l2_op(n1, n2))
+
+	# implement all vector operators used in VERSE experiments for calculating edgewise embeddings
+	@staticmethod
+	def average_op(n1, n2):
+		return (n1 + n2) / 2
+
+	@staticmethod
+	def concat_op(n1, n2):
+		return np.concatenate((n1, n2), axis=0)
+
+	@staticmethod
+	def hadamard_op(n1, n2):
+		return n1 * n2
+
+	@staticmethod
+	def weighted_l1_op(n1, n2):
+		return n1 - n2
+
+	@staticmethod
+	def weighted_l2_op(n1, n2):
+		return np.square(n1 - n2)
+
+	# train through logistic regression
+	def train(self):
+		print('Train multi-class classification experiment with {} on {} evaluated through {} on {}% train data!'
+			  .format(self.method_name, self.dataset_name, self.performance_function, self.train_size * 100.00))
+
+		self.start_time = time.time()
+
+		self.logistic_regression_model = LogisticRegression(penalty='l2', C=1., multi_class='multinomial',
+															solver='saga',
+															verbose=1, class_weight='balanced')
+		self.logistic_regression_model.fit(self.embeddings_train, self.node_labels_train)
+
+		self.end_time = time.time()
+
+		total_train_time = round(self.end_time - self.start_time, 2)
+		print('Trained multi-class classification experiment in {} sec.!'.format(total_train_time))
+
+		return self.logistic_regression_model
+
+	# predict class of each sample, based on pre-trained model
+	def predict(self):
+		print('Predict multi-class classification experiment with {} on {} evaluated through {} on {}% train data!'
+			  .format(self.method_name, self.dataset_name, self.performance_function, self.train_size * 100.00))
+
+		self.start_time = time.time()
+
+		self.node_label_predictions = self.logistic_regression_model.predict(self.embeddings_test)
+
+		self.end_time = time.time()
+
+		total_prediction_time = round(self.end_time - self.start_time, 2)
+		print('Predicted multi-class classification experiment in {} sec.!'.format(total_prediction_time))
+
+		return self.node_label_predictions
+
+	# evaluate prediction results through already pre-defined performance function(s), return results as a dict
+	def evaluate(self):
+		print('Evaluate multi-class classification experiment with {} on {} evaluated through {} on {}% train data!'
+			  .format(self.method_name, self.dataset_name, self.performance_function, self.train_size * 100.00))
+
+		results = {}
+
+		if self.performance_function == self.BOTH:
+			results['macro'] = f1_score(self.node_labels_test, self.node_label_predictions, average='macro')
+			results['micro'] = f1_score(self.node_labels_test, self.node_label_predictions, average='micro')
+		elif self.performance_function == self.MACRO_F1:
+			results['macro'] = f1_score(self.node_labels_test, self.node_label_predictions, average='macro')
+		elif self.performance_function == self.MICRO_F1:
+			results['micro'] = f1_score(self.node_labels_test, self.node_label_predictions, average='micro')
+
+		return results
 
 
 # multi-class classification through logistic regression
@@ -111,7 +261,7 @@ class MultiClassClassification:
 		return results
 
 
-# multi-label classification through k-nearest neighbor classifier
+# multi-label classification through k-nearest neighbor classifier or one vs. rest logistic regression
 # wrapper for customizable initialization, training, prediction and evaluation
 class MultiLabelClassification:
 	start_time = None
