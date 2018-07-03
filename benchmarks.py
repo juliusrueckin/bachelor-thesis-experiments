3d722faae2c68d1f294a615398529ec468650dd1
Julius Rückin <julius.rueckin@student.hpi.uni-potsdam.de>
2018-07-03 19:23:07 +0200

    add one vs rest logistic regression for multi-label classification

--- benchmarks.py
+++ benchmarks.py
@@ -7,6 +7,7 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.multiclass import OneVsRestClassifier
 
 
 # multi-class classification through logistic regression
@@ -122,11 +123,16 @@ class MultiLabelClassification:
 	performance_function = 'Macro-F1'
 	train_size = 0.3
 	n_neighbors = 5
+	chosen_classifier = ''
 
 	# performance evaluation methods
 	MACRO_F1 = 'macro_f1'
 	MICRO_F1 = 'micro_f1'
 	BOTH = 'both'
+
+	# classifiers
+	NEAREST_NEIGHBOR = 'nearest_neighbor'
+	LOGISTIC_REGRESSION = 'logistic_regression'
 
 	# feature and label space, where each sample has a set of classes
 	embeddings = []
@@ -139,13 +145,13 @@ class MultiLabelClassification:
 	node_labels_test = []
 
 	# model and its prediction
-	nearest_neighbors = None
+	multi_label_model = None
 	node_label_predictions = []
 
 	# initialize classification algorithm with customized configuration parameters
 	# produce random train-test split
 	def __init__(self, method_name='Verse-PPR', dataset_name='Test-Data', performance_function='both',
-				 train_size=0.3, embeddings=None, node_labels=None, n_neighbors=5):
+				 train_size=0.3, embeddings=None, node_labels=None, n_neighbors=5, classifier='logistic_regression'):
 		print('Initialize multi-label classification experiment with {} on {} evaluated through {} on {}% train data!'
 			  .format(method_name, dataset_name, performance_function, train_size * 100.00))
 
@@ -156,6 +162,7 @@ class MultiLabelClassification:
 		self.embeddings = embeddings
 		self.node_labels = MultiLabelBinarizer().fit_transform(node_labels)
 		self.n_neighbors = n_neighbors
+		self.chosen_classifier = classifier
 
 		self.embeddings_train, self.embeddings_test, self.node_labels_train, self.node_labels_test = \
 			train_test_split(self.embeddings, self.node_labels, train_size=train_size, test_size=1 - train_size)
@@ -167,16 +174,23 @@ class MultiLabelClassification:
 
 		self.start_time = time.time()
 
-		self.nearest_neighbors = KNeighborsClassifier(n_neighbors=self.n_neighbors, weights='distance', algorithm='auto',
-													  metric='cosine', n_jobs=-1)
-		self.nearest_neighbors.fit(self.embeddings_train, self.node_labels_train)
+		if self.chosen_classifier == self.NEAREST_NEIGHBOR:
+			self.multi_label_model = KNeighborsClassifier(n_neighbors=self.n_neighbors, weights='distance',
+														  algorithm='auto', metric='cosine', n_jobs=-1)
+		elif self.chosen_classifier == self.LOGISTIC_REGRESSION:
+			self.multi_label_model = \
+				OneVsRestClassifier(
+					LogisticRegression(penalty='l2', C=1., multi_class='multinomial', solver='saga',
+									   verbose=1, class_weight='balanced'), n_jobs=-1)
+
+		self.multi_label_model.fit(self.embeddings_train, self.node_labels_train)
 
 		self.end_time = time.time()
 
 		total_train_time = round(self.end_time - self.start_time, 2)
 		print('Trained multi-label classification experiment in {} sec.!'.format(total_train_time))
 
-		return self.nearest_neighbors
+		return self.multi_label_model
 
 	# predict class of each sample, based on pre-trained model
 	def predict(self):
@@ -185,7 +199,7 @@ class MultiLabelClassification:
 
 		self.start_time = time.time()
 
-		self.node_label_predictions = self.nearest_neighbors.predict(self.embeddings_test)
+		self.node_label_predictions = self.multi_label_model.predict(self.embeddings_test)
 
 		self.end_time = time.time()
 
