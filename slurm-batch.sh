6039b49a454dd70e4465563366cf2eeb665cf6b1
Sebastian Bischoff <sebastian.bischoff@vm-fsoc-submit-001.fsoc.hpi.uni-potsdam.de>
2018-07-19 13:55:08 +0200

    stop and remove docker container

--- slurm-batch.sh
+++ slurm-batch.sh
@@ -2,6 +2,8 @@
 #SBATCH --cpus 80 
 #SBATCH --mem 100GB 
 #SBATCH --job-name blog-catalog-preprocessing-v1 
+docker stop julius-experiments-container
+docker rm julius-experiments-container
 docker build --tag julius-experiments -f Dockerfile . 
 docker run -d --name julius-experiments-container julius-experiments
 docker exec julius-experiments-container python3 /bachelor-thesis-experiments/preprocessing-blogcatalog.py
