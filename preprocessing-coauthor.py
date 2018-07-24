e98f16c20c2983daebbb7444d86fb0c963c96b1f
Julius Rückin <julius.rueckin@student.hpi.uni-potsdam.de>
2018-07-24 18:10:46 +0200

    fix index bug

--- preprocessing-coauthor.py
+++ preprocessing-coauthor.py
@@ -150,7 +150,7 @@ lower_partition_index = partition_id * num_nodes_per_partition
 upper_partition_index = (partition_id + 1) * num_nodes_per_partition
 nodes_list = list(coauthor_graph.nodes)[lower_partition_index:upper_partition_index]
 print("Compute from node {}|{} to {}|{}".format(lower_partition_index, nodes_list[0],
-												upper_partition_index-1, nodes_list[lower_partition_index-1]))
+												upper_partition_index-1, nodes_list[-1]))
 start_time = time.time()
 
 with Pool(cpu_count()) as pool:
